d0027c1f385622b589908d2b197d983a2061cf74
Samara Stewart <stewartsamara24@gmail.com>
2026-04-07 23:47:34 -0500

    Created Movies Database table

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
-from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-from app import views
+app.config['SECRET_KEY'] = 'Som3$ec5etK*y'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%40Ginger25@localhost/lab5'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
+
+from app import views, models
+ # noqa: E402, F401
--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
-# Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+
+    def __repr__(self):
+        return f'<Movie id={self.id} title={self.title!r}>'
